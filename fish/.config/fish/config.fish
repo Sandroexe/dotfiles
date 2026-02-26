25fb096f337e83618ebbb9e6de62c00119bf2c56
Sandroexe <sandro.exenberger@gmail.com>
2026-02-26 23:48:18 +0100

    🚀 Initial commit: Fish shell config added

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,10 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+# OpenClaw Completion
+source "/home/sandro/.openclaw/completions/openclaw.fish"
